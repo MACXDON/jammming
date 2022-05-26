8415b0d8de7b6e2bae271972071291ca60603841
MACXDON <72674976+MACXDON@users.noreply.github.com>
2022-05-26 00:47:21 -0500

    addTrack and removeTrack
    Added addTrack and removeTrack functionality. I also figured out why i couldn't map over the array. There's a Tracklist component in the Playlist component that was missing an array property. Hopefully I remember that next time.

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -28,8 +28,54 @@ class App extends React.Component {
           album: 'album3',
           id: 3
         }
+      ],
+
+      playlistName: 'My Playlist',
+
+      playlistTracks: [
+        {
+          name: 'playlistName1',
+          artist: 'playlistArtist1',
+          album: 'playlistAlbum1',
+          id: 4
+        },
+        {
+          name: 'playlistName2',
+          artist: 'playlistArtist2',
+          album: 'playlistAlbum2',
+          id: 5
+        },
+        {
+          name: 'playlistName3',
+          artist: 'playlistArtist3',
+          album: 'playlistAlbum3',
+          id: 6
+        }
       ]
     }
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    if(tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return
+    }
+    
+    tracks.push(track);
+
+    this.setState({ playlistTracks: tracks })
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -39,8 +85,15 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <SearchResults 
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist 
+              playlistName={this.state.playlistName} 
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+            />
           </div>
         </div>
       </div>
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -5,19 +5,19 @@ import Track from "../Track/Track";
 
 export class TrackList extends React.Component {
     render() {
-        console.log(typeof this.props.tracks, this.props.tracks);
-
         return (
             <div className="TrackList">
                 {
-                    // this.props.tracks.map(track => {
-                    //     return (
-                    //         <Track 
-                    //             key={track.id}
-                    //             track={track}
-                    //         />
-                    //     )
-                    // })
+                    this.props.tracks.map(track => {
+                        return  <Track 
+                                    key={track.id}
+                                    track={track}
+                                    onAdd={this.props.onAdd}
+                                    onRemove={this.props.onRemove}
+                                    isRemoval={this.props.isRemoval}
+                                />
+                        
+                    })
                 }
             </div>
         )
